e6cae7c4cca9ea61dbc74ee27160c4f15a740abc
mzemskov <mzemskov@dimax.digital>
2022-12-24 10:19:52 +0300

    task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int sum = 0;
+
+int rec(int a, int b)
+{
+if (a > b)
+return sum;
+else
+{
+	sum += a;
+	return rec(a + 1, b);
+}
+
+}
+
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
